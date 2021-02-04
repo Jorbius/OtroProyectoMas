80669f6f31375112891c38c45c78d26ca290db12
Borja Pecharroman <bpecharroman002@ikasle.ehu.eus>
2021-02-04 16:53:06 +0100

    Versión inicial

---
+++ src/MiPrimeraClase.java
@@ -0,0 +1,13 @@
+
+public class MiPrimeraClase {
+
+	public static void main(String[] args) {
+		System.out.println("Estos es un mensaje");
+		otroMetodo();
+	}
+	
+	private static void otroMetodo() {
+		System.out.println("Otro método");
+	}
+
+}
